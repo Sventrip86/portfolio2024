d1e0a98ea448dc09ac298bc9deaa952605a76a08
Sventrip86 <fab.sebis@gmail.com>
2024-05-11 19:24:21 +0200

    added app bar with drawer for mobile and started intro component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import Nav from './components/Nav';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {  yellow } from '@mui/material/colors';
+import Home from './components/Home';
 
-function App() {
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFEE59',
+    },
+    secondary: yellow,
+  },
+});
+
+const App = () => {
   return (
-    <div>
-      <h1>fabs portfolio</h1>
-    </div>
-  );
+    <>
+     <ThemeProvider theme={theme}>
+<Nav></Nav>
+<Home/>
+</ThemeProvider>
+    </>
+  )
 }
 
 export default App;
